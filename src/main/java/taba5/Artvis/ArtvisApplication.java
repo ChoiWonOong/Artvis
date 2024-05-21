566cfc38f298ebe3fe900acb97e95d6ac1d63750
ChoiWonOong <dnd5301@gmail.com>
2024-05-21 09:56:08 +0900

    initial commit

---
+++ src/main/java/taba5/Artvis/ArtvisApplication.java
@@ -0,0 +1,13 @@
+package taba5.Artvis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtvisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtvisApplication.class, args);
+	}
+
+}
