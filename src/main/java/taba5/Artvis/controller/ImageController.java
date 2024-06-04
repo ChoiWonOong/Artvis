912014ec48a8785543cab99cd5d6b2b7d39a5b45
ChoiWonOong <dnd5301@gmail.com>
2024-06-04 22:02:40 +0900

    FEAT: Add Get Exhibition Count,Avg

--- src/main/java/taba5/Artvis/controller/ImageController.java
+++ src/main/java/taba5/Artvis/controller/ImageController.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import taba5.Artvis.dto.Exhibition.ExhibitionRequestDto;
 import taba5.Artvis.dto.Image.ImageUploadDto;
 import taba5.Artvis.service.ImageService;
 
@@ -16,8 +18,8 @@ public class ImageController {
     private final ImageService imageService;
 
     @PostMapping("/save")
-    public ResponseEntity<Long> saveImage(@ModelAttribute ImageUploadDto imageUploadDto) {
-        return ResponseEntity.ok(imageService.saveImage(imageUploadDto));
+    public ResponseEntity<Long> saveImage(@RequestPart(value = "image")MultipartFile image, @RequestPart(value = "request") ExhibitionRequestDto exhibitionRequestDto) {
+        return ResponseEntity.ok(imageService.saveImage(image, exhibitionRequestDto));
     }
     @GetMapping("/{id}")
     public ResponseEntity<Resource> loadImage(@PathVariable Long id) throws IOException {
--- src/main/java/taba5/Artvis/service/ImageService.java
+++ src/main/java/taba5/Artvis/service/ImageService.java
@@ -2,6 +2,7 @@ package taba5.Artvis.service;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ContentDisposition;
@@ -12,9 +13,15 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import taba5.Artvis.Exception.ErrorCode;
 import taba5.Artvis.Exception.RestApiException;
+import taba5.Artvis.domain.Detail;
+import taba5.Artvis.domain.Exhibition.Exhibition;
+import taba5.Artvis.domain.Exhibition.ExhibitionTag;
+import taba5.Artvis.domain.Exhibition.Tag;
 import taba5.Artvis.domain.Image;
+import taba5.Artvis.dto.DetailDto;
+import taba5.Artvis.dto.Exhibition.ExhibitionRequestDto;
 import taba5.Artvis.dto.Image.ImageUploadDto;
-import taba5.Artvis.repository.ImageRepository;
+import taba5.Artvis.repository.*;
 
 import java.io.File;
 import java.io.IOException;
@@ -22,19 +29,40 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.UUID;
-
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ImageService {
     private final ImageRepository imageRepository;
+    private final DetailRepository detailRepository;
+    private final ExhibitionRepository exhibitionRepository;
+    private final TagRepository tagRepository;
+    private final ExhibitionTagRepository exhibitionTagRepository;
+
+
     private final String uploadFolder = "C:\\Artvis\\Image\\";
     @Transactional
-    public Long saveImage(ImageUploadDto imageUploadDTO) {
-
+    public Long saveImage(MultipartFile file, ExhibitionRequestDto exhibitionRequestDto) {
+        // Create Exhibition
+        log.info("detailList : " + exhibitionRequestDto.getDetailList().stream().map(DetailDto::getContents).toList());
+        List<Detail> detailList = Detail.toEntityList(exhibitionRequestDto.getDetailList());
+        detailRepository.saveAll(detailList);
+        Exhibition exhibition = new Exhibition(
+                exhibitionRequestDto.getTitle(),
+                exhibitionRequestDto.getLocation(),
+                exhibitionRequestDto.getStartDate(),
+                exhibitionRequestDto.getEndDate(),
+                detailList);
+        exhibitionRepository.save(exhibition);
+        for(String tagName : exhibitionRequestDto.getExhibitionTagList()){
+            Tag tag = tagRepository.findByTagName(tagName)
+                    .orElseThrow(()->new RestApiException(ErrorCode.NOT_EXIST_ERROR));
+            createExhibitionTag(exhibition, tag);
+        }
         Image image = null;
-        if (imageUploadDTO.getFile() != null && !imageUploadDTO.getFile().isEmpty()) {
-            MultipartFile file = imageUploadDTO.getFile();
+        if (file != null && !file.isEmpty()) {
             UUID uuid = UUID.randomUUID();
             String imageFileName = uuid + "_" + file.getOriginalFilename();
 
@@ -55,6 +83,13 @@ public class ImageService {
         assert image != null;
         Image result = imageRepository.findByUrl(image.getUrl()).orElseThrow(RuntimeException::new);
         return image.getId();
+    }
+    public void createExhibitionTag(Exhibition exhibition, Tag tag){
+        ExhibitionTag exhibitionTag = ExhibitionTag.builder()
+                .exhibition(exhibition)
+                .tag(tag)
+                .build();
+        exhibitionTagRepository.save(exhibitionTag);
     }
     public ResponseEntity<Resource> loadImage(Long id) throws IOException {
         Image image = imageRepository.findById(id)
